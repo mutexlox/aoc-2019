4a8e7a7e542ed6995eb677b6affbd7a42146e806
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2021-01-16 15:53:51 -0500

    Do day1 pt 2

--- src/day01_rocket_equation.rs
+++ src/day01_rocket_equation.rs
@@ -5,10 +5,33 @@ fn fuel_to_launch(mass: i32) -> i32 {
     mass / 3 - 2
 }
 
-fn total_fuel_requirement(module_masses: Vec<i32>) -> i32 {
+fn fuel_to_launch_counting_fuel(mass: i32) -> i32 {
+    let base = fuel_to_launch(mass);
+    let mut additional = 0;
+    let mut last = base;
+    while last > 0 {
+        let new = fuel_to_launch(last);
+        if new >= 0 {
+            additional += new
+        }
+        last = new;
+    }
+
+    base + additional
+}
+
+fn total_fuel_requirement(module_masses: &[i32]) -> i32 {
     let mut out = 0;
     for mass in module_masses {
-       out += fuel_to_launch(mass);
+        out += fuel_to_launch(*mass);
+    }
+    out
+}
+
+fn total_fuel_requirement_counting_fuel(module_masses: &[i32]) -> i32 {
+    let mut out = 0;
+    for mass in module_masses {
+        out += fuel_to_launch_counting_fuel(*mass);
     }
     out
 }
@@ -18,8 +41,13 @@ fn main() {
     assert_eq!(args.len(), 2);
 
     let input: String = fs::read_to_string(&args[1]).expect("couldn't read file");
-    let lines = input.split_whitespace().map(|s| {
-        s.parse::<i32>().unwrap_or_else(|_| panic!("invalid int {}", &s))
-    }).collect();
-    println!("{}", total_fuel_requirement(lines));
+    let lines = input
+        .split_whitespace()
+        .map(|s| {
+            s.parse::<i32>()
+                .unwrap_or_else(|_| panic!("invalid int {}", &s))
+        })
+        .collect::<Vec<_>>();
+    println!("{}", total_fuel_requirement(&lines));
+    println!("{}", total_fuel_requirement_counting_fuel(&lines));
 }
