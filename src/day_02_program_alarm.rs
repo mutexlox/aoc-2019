56df37f19a6ea56ef5e9e06effd975847c53c346
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2021-01-16 16:41:08 -0500

    do day 2 part 2

--- src/day_02_program_alarm.rs
+++ src/day_02_program_alarm.rs
@@ -4,12 +4,26 @@ use crate::intcode::eval;
 use std::env;
 use std::fs;
 
+fn find_noun_and_verb(ints: &[usize]) -> (usize, usize) {
+    for i in 0..100 {
+        for j in 0..100 {
+            let mut c = ints.to_owned();
+            c[1] = i;
+            c[2] = j;
+            if eval(&mut c) == 19690720 {
+                return (i, j);
+            }
+        }
+    }
+    panic!("no pair found");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert_eq!(args.len(), 2);
 
     let input: String = fs::read_to_string(&args[1]).expect("couldn't read file");
-    let mut ints = input
+    let ints = input
         .split(',')
         .map(|s| {
             s.trim()
@@ -17,7 +31,11 @@ fn main() {
                 .unwrap_or_else(|_| panic!("invalid int {}", s))
         })
         .collect::<Vec<_>>();
-    ints[1] = 12;
-    ints[2] = 2;
-    println!("{}", eval(&mut ints));
+    let mut mem = ints.clone();
+    mem[1] = 12;
+    mem[2] = 2;
+
+    println!("{}", eval(&mut mem));
+    let (noun, verb) = find_noun_and_verb(&ints);
+    println!("{}", 100 * noun + verb);
 }
