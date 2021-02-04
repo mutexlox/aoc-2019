72e5c562586ae4d4f4d41f00f21f09b6af3016c2
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2021-02-04 14:36:55 -0500

    Do day 11 part 2

--- src/day11_paint_intcode.rs
+++ src/day11_paint_intcode.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::sync::mpsc;
 use std::thread;
 
-fn count_panels_painted(ints: &HashMap<usize, i64>) -> usize {
+fn count_panels_painted(ints: &HashMap<usize, i64>, tiles: &mut HashSet<(i64, i64)>) -> usize {
     let (in_sender, in_receiver) = mpsc::channel();
     let (out_sender, out_reciever) = mpsc::channel();
 
@@ -15,7 +15,6 @@ fn count_panels_painted(ints: &HashMap<usize, i64>) -> usize {
         out_sender.send(-1).unwrap();
     });
 
-    let mut tiles = HashSet::new();
     let mut loc_x = 0;
     let mut loc_y = 0;
     let mut face = (0, 1);
@@ -64,11 +63,54 @@ fn count_panels_painted(ints: &HashMap<usize, i64>) -> usize {
     painted.len()
 }
 
+fn print_tiles(tiles: &HashSet<(i64, i64)>) {
+    let mut min_x = 0;
+    let mut max_x = 0;
+    let mut min_y = 0;
+    let mut max_y = 0;
+    for (i, (x, y)) in tiles.iter().enumerate() {
+        if i == 0 {
+            min_x = *x;
+            max_x = *x;
+            min_y = *y;
+            max_y = *y;
+        }
+        if min_x > *x {
+            min_x = *x;
+        }
+        if max_x < *x {
+            max_x = *x;
+        }
+        if min_y > *y {
+            min_y = *y;
+        }
+        if max_y < *y {
+            max_y = *y;
+        }
+    }
+    for x in min_x..max_x + 1 {
+        for y in min_y..max_y + 1 {
+            if tiles.contains(&(x, y)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert_eq!(args.len(), 2);
     let input = fs::read_to_string(&args[1]).expect("couldn't read file");
     let ints = intcode::parse(&input);
 
-    println!("{}", count_panels_painted(&ints));
+    let mut tiles = HashSet::new();
+    println!("{}", count_panels_painted(&ints, &mut tiles));
+
+    tiles = HashSet::new();
+    tiles.insert((0, 0));
+    count_panels_painted(&ints, &mut tiles);
+    print_tiles(&tiles);
 }
